b1b88b48aba00c12139131fd210dab7651e682de
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-03 18:09:25 +0900

    delete printf

--- srcs/main.c
+++ srcs/main.c
@@ -20,7 +20,6 @@ void	ft_update_fractal(t_data *data)
 		return ;
 	x_cp = ((data->max_r - data->min_r) * (double)x) / WIDTH + data->min_r;
 	y_cp = ((data->max_i - data->min_i) * (double)y) / HEIGHT + data->min_i;
-	printf("x: %f, y: %f\n", x_cp, y_cp);
 	data->c_r = x_cp;
 	data->c_i = y_cp;
 }
